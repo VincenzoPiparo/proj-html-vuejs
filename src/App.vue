f8b6a892611e2595a27e4506a98487a7bc465fbe
VincenzoPiparo <v.pipar.97@gmai.com>
2022-01-17 18:09:25 +0100

    END HEADER

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :type="navArray" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-
 export default {
   name: "App",
   components: {
     Header,
+  },
+  data: function () {
+    return {
+      navArray: [
+        {
+          text: "Home",
+          current: true,
+        },
+        {
+          text: "About us",
+          current: false,
+        },
+        {
+          text: "Feature",
+          current: false,
+        },
+        {
+          text: "Testimonials",
+          current: false,
+        },
+        {
+          text: "Contact US",
+          current: false,
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
 @import "./styles/general";
+@import "../node_modules/bootstrap";
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,50 @@
 <template>
   <header>
-    <h1>ESEMPIO</h1>
+    <div class="container">
+      <div class="d-flex justify-content-center">
+        <div class="logo mx-5 col-3">
+          <img src="../assets/img/logo-2.png" alt="" />
+        </div>
+        <div class="navbar col-9">
+          <div
+            :class="element.current ? 'current' : null"
+            v-for="(element, index) in type"
+            :key="index"
+          >
+            {{ element.text }}
+          </div>
+          <!-- FONTAWESOME ICONS -->
+          <div>
+            <i class="fas fa-search px-5"></i>
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+          <!-- BUTTON HEADER -->
+          <button type="button" class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
+export default {
+  name: "Header",
+  props: {
+    type: Array,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+header {
+  margin-top: 20px;
+  width: 100%;
+  img {
+    width: 70px;
+  }
+  .current {
+    // CAMBIARE IL COLORE IN BLUE
+    color: blue;
+  }
+}
 </style>

f2bbe0cbfaa5cbc99aa344f0408b51ff57fb3dec
VincenzoPiparo <v.pipar.97@gmai.com>
2022-01-18 17:43:46 +0100

    ADD ANALYSIS

---
+++ src/components/Analysis.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <div class="container wrapper">
+      <!-- TOP COL -->
+      <div class="col-6">
+        <h2>Get tips tricks on how to skyrocket your sales.</h2>
+        <span
+          >Faff about only a quid blower i don't want no agro bleeding chimney
+          pot burke tosser cras nice boot fannt.!</span
+        >
+        <div class="shadow-lg p-3 mt-5 mb-5 bg-white rounded">
+          <div class="d-flex align-items-center">
+            <img src="../assets/img/510.png" alt="statistics" />
+            <div class="mx-5">
+              <h4 class="px-6">Reporting Analysis</h4>
+              <p class="">
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when at its layout.
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- BOTTOM COL -->
+        <div class="d-flex align-items-center">
+          <img src="../assets/img/511.png" alt="money" />
+          <div class="mx-5">
+            <h4 class="px-6">Techincal SEO Audit</h4>
+            <p class="">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when at its layout.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- IMG -->
+      <div class="px-5">
+        <img src="../assets/img/509.jpeg" alt="" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+// DARE STILI
+section {
+  .wrapper {
+    display: flex;
+    margin-top: 200px;
+  }
+}
+</style>

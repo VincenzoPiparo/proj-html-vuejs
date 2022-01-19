8b389d7e109d5d05765412dd43f349c98adb5d8a
VincenzoPiparo <v.pipar.97@gmai.com>
2022-01-19 14:46:31 +0100

    ADD POST

---
+++ src/components/Post.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <div class="container my_container">
+      <!-- CARD LEFT -->
+      <div class="card" style="width: 18rem">
+        <img src="../assets/img/19.jpg" class="card-img-top" alt="clock" />
+        <div class="card-body">
+          <h5 class="card-title">Life Lack Meaning</h5>
+          <p class="card-text">
+            It is a long established fact that a reader will be distracted by
+            the readable
+          </p>
+        </div>
+      </div>
+      <!-- CARD CENTER -->
+      <div class="card" style="width: 18rem">
+        <img src="../assets/img/18.jpg" class="card-img-top" alt="clock" />
+        <div class="card-body">
+          <h5 class="card-title">Life Lack Meaning</h5>
+          <p class="card-text">
+            It is a long established fact that a reader will be distracted by
+            the readable
+          </p>
+        </div>
+      </div>
+      <!-- CARD RIGHT-->
+      <div class="card" style="width: 18rem">
+        <img src="../assets/img/17.jpg" class="card-img-top" alt="clock" />
+        <div class="card-body">
+          <h5 class="card-title">Life Lack Meaning</h5>
+          <p class="card-text">
+            It is a long established fact that a reader will be distracted by
+            the readable
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.my_container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 150px;
+}
+</style>

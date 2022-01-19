062d55988a59f1cf49345290ea63e42aa047a0d7
VincenzoPiparo <v.pipar.97@gmai.com>
2022-01-19 17:32:17 +0100

    ADD BACKGROUND

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,18 +1,23 @@
 <template>
   <section>
-    <div class="container">
-      <div
-        :class="element.current ? 'current' : null"
-        v-for="(element, index) in type"
-        :key="index"
-        class="card mt-3"
-        style="width: 18rem"
-      >
-        <div class="card-body">
-          <h5 class="card-title">
-            {{ element.text }}
-          </h5>
+    <div class="position-relative">
+      <div class="container">
+        <div
+          :class="element.current ? 'current' : null"
+          v-for="(element, index) in type"
+          :key="index"
+          class="card mt-3"
+          style="width: 18rem"
+        >
+          <div class="card-body">
+            <h5 class="card-title">
+              {{ element.text }}
+            </h5>
+          </div>
         </div>
+      </div>
+      <div class="background_circle">
+        <img src="../assets/img/14.png" alt="circle" />
       </div>
     </div>
   </section>
@@ -28,6 +33,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/variables";
+.background_circle {
+  position: absolute;
+  left: -600px;
+  top: -300px;
+}
 .container {
   display: flex;
   justify-content: space-around;

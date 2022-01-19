3d002d755208aed33a43cd7706225a8ae074eed2
VincenzoPiparo <v.pipar.97@gmai.com>
2022-01-19 12:10:24 +0100

    ADD CARDS

---
+++ src/components/Cards.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <div class="container">
+      <div
+        :class="element.current ? 'current' : null"
+        v-for="(element, index) in type"
+        :key="index"
+        class="card mt-3"
+        style="width: 18rem"
+      >
+        <div class="card-body">
+          <h5 class="card-title">
+            {{ element.text }}
+          </h5>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Cards",
+  props: {
+    type: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
